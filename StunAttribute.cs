03efe029d69fdda075b7f47cd2c2ad10016badfd
kadoshita <sublimer.me@gmail.com>
2021-11-02 19:44:07 +0900

    Add stun error code attribute class

--- StunAttribute.cs
+++ StunAttribute.cs
@@ -32,4 +32,44 @@ namespace doturn
             return res;
         }
     }
+    class StunAttributeErrorCode : IStunAttribute
+    {
+        public readonly StunAttrType attrType = StunAttrType.ERROR_CODE;
+        public readonly byte errorClass;
+        public readonly byte errorCode;
+        public readonly string errorReasonPhrase;
+
+        public StunAttributeErrorCode(byte errorClass, byte errorCode, string errorReasonPhrase)
+        {
+            this.errorClass = errorClass;
+            this.errorCode = errorCode;
+            this.errorReasonPhrase = errorReasonPhrase;
+        }
+        public byte[] ToByte()
+        {
+            byte[] errorByte = { this.errorClass, this.errorCode };
+            var errorReasonPhraseByte = System.Text.Encoding.ASCII.GetBytes(this.errorReasonPhrase);
+            var reserved = BitConverter.GetBytes((Int16)0x0000);
+            var length = errorByte.Length + errorReasonPhraseByte.Length + reserved.Length;
+            var lengthByte = BitConverter.GetBytes((Int16)length);
+            var attrTypeByte = this.attrType.ToByte();
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(lengthByte);
+            }
+
+            var res = new byte[2 + 2 + length];
+            int endPos = 0;
+            Array.Copy(attrTypeByte, 0, res, endPos, attrTypeByte.Length);
+            endPos += attrTypeByte.Length;
+            Array.Copy(lengthByte, 0, res, endPos, lengthByte.Length);
+            endPos += lengthByte.Length;
+            Array.Copy(reserved, 0, res, endPos, reserved.Length);
+            endPos += reserved.Length;
+            Array.Copy(errorByte, 0, res, endPos, errorByte.Length);
+            endPos += errorByte.Length;
+            Array.Copy(errorReasonPhraseByte, 0, res, endPos, errorReasonPhraseByte.Length);
+            return res;
+        }
+    }
 }
