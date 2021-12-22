f81db8ca386a63a0016b2a2ebc6bbcca578e3b3f
kadoshita <sublimer.me@gmail.com>
2021-12-22 21:36:55 +0900

    Add MergeByteArray method in common package

---
+++ src/Doturn.Common/ByteArrayUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Doturn.Common
+{
+    public static class ByteArrayUtils
+    {
+        public static void MergeByteArray(ref byte[] res, params byte[][] data)
+        {
+            int endPos = 0;
+            foreach (var datum in data)
+            {
+                Buffer.BlockCopy(datum, 0, res, endPos, datum.Length);
+                endPos += datum.Length;
+            }
+        }
+    }
+}
