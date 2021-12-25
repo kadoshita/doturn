39f5e8d8feb952b7d045674d7cb5b86c1c254f37
kadoshita <sublimer.me@gmail.com>
2021-12-25 23:25:39 +0900

    Add message integrity stun attribute

---
+++ src/Doturn.StunAttribute/MessageIntegrity.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Security.Cryptography;
+using Doturn.Common;
+
+namespace Doturn.StunAttribute
+{
+    public class MessageIntegrity : StunAttributeBase
+    {
+        public readonly Type type = Type.MESSAGE_INTEGRITY;
+        private readonly byte[] messageIntegrity;
+        public override Type Type => this.type;
+
+        public MessageIntegrity(byte[] messageIntegrity)
+        {
+            this.messageIntegrity = messageIntegrity;
+        }
+        public MessageIntegrity(string username, string password, string realm, byte[] data)
+        {
+            var md5 = MD5.Create();
+            var keyString = $"{username}:{realm}:{password}";
+            var keyStringByteArray = System.Text.Encoding.ASCII.GetBytes(keyString);
+            var md5HashByteArray = md5.ComputeHash(keyStringByteArray);
+            var hmacSHA1 = new HMACSHA1(md5HashByteArray);
+            md5.Clear();
+            var hmacSHA1ByteArray = hmacSHA1.ComputeHash(data);
+            this.messageIntegrity = hmacSHA1ByteArray;
+            hmacSHA1.Clear();
+        }
+        public override byte[] ToByte()
+        {
+            var typeByteArray = this.type.ToByte();
+            var length = this.messageIntegrity.Length;
+            var lengthByteArray = BitConverter.GetBytes((Int16)length);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(lengthByteArray);
+            }
+
+            var res = new byte[2 + 2 + length];
+            ByteArrayUtils.MergeByteArray(ref res, typeByteArray, lengthByteArray, this.messageIntegrity);
+            return res;
+        }
+    }
+}
