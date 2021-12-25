4cf8abd41360838d481697a6c4f3b7ce212a5381
kadoshita <sublimer.me@gmail.com>
2021-12-25 23:23:50 +0900

    Add username stun attribute

---
+++ src/Doturn.StunAttribute/Username.cs
@@ -0,0 +1,40 @@
+using System;
+using Doturn.Common;
+
+namespace Doturn.StunAttribute
+{
+    public class UsernameIsEmptyException : Exception
+    {
+        public UsernameIsEmptyException() : base() { }
+    }
+    public class Username : StunAttributeBase
+    {
+        public readonly Type type = Type.USERNAME;
+        public readonly string username;
+        public override Type Type => this.type;
+
+        public Username(string username)
+        {
+            if (String.IsNullOrEmpty(username))
+            {
+                throw new UsernameIsEmptyException();
+            }
+            this.username = username;
+        }
+
+        public override byte[] ToByte()
+        {
+            var typeByteArray = this.type.ToByte();
+            byte[] usernameByteArray = System.Text.Encoding.ASCII.GetBytes(this.username);
+            var length = usernameByteArray.Length;
+            var lengthByteArray = BitConverter.GetBytes((Int16)length);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(lengthByteArray);
+            }
+            var res = new byte[2 + 2 + length];
+            ByteArrayUtils.MergeByteArray(ref res, typeByteArray, lengthByteArray, usernameByteArray);
+            return res;
+        }
+    }
+}
---
+++ test/Doturn.StunAttribute.Test/UsernameTest.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace Doturn.StunAttribute.Test
+{
+    public class UsernameTest
+    {
+        [Theory]
+        [InlineData("username", "00-06-00-08-75-73-65-72-6E-61-6D-65")]
+        [InlineData("bob", "00-06-00-03-62-6F-62")]
+        [InlineData("alice", "00-06-00-05-61-6C-69-63-65")]
+        public void Username_Convert_To_ByteArray(string name, string expect)
+        {
+            var username = new Username(name);
+            var byteArray = username.ToByte();
+            var byteArrayString = BitConverter.ToString(byteArray);
+            Assert.Equal(expect, byteArrayString);
+        }
+
+        [Fact]
+        public void Throw_Exception_If_Username_Is_Empty()
+        {
+            Assert.Throws<UsernameIsEmptyException>(() => new Username(""));
+        }
+    }
+}
