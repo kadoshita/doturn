5decfa87c31c1e86bb9cf29392506314cc2ea64e
kadoshita <sublimer.me@gmail.com>
2021-12-26 20:35:50 +0900

    Add Nonce stun attribute

---
+++ src/Doturn.StunAttribute/Nonce.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Linq;
+using Doturn.Common;
+
+namespace Doturn.StunAttribute
+{
+    public class Nonce : StunAttributeBase
+    {
+        public readonly Type type = Type.NONCE;
+        public readonly string nonce;
+        private static Random random = new Random();
+        public override Type Type => this.type;
+        public Nonce()
+        {
+            this.nonce = this.generateNonce(16);
+        }
+        public Nonce(string nonce)
+        {
+            this.nonce = nonce;
+        }
+
+        public override byte[] ToByte()
+        {
+            var typeByteArray = this.type.ToByte();
+            var nonceByteArray = System.Text.Encoding.ASCII.GetBytes(this.nonce);
+            var length = nonceByteArray.Length;
+            var lengthByteArray = BitConverter.GetBytes((Int16)length);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(lengthByteArray);
+            }
+            var res = new byte[2 + 2 + length];
+            ByteArrayUtils.MergeByteArray(ref res, typeByteArray, lengthByteArray, nonceByteArray);
+            return res;
+        }
+        private string generateNonce(int length)
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ test/Doturn.StunAttribute.Test/NonceTest.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+
+namespace Doturn.StunAttribute.Test
+{
+    public class NonceTest
+    {
+        [Theory]
+        [InlineData("ho2ydw5qeeqsgasz", "00-15-00-10-68-6F-32-79-64-77-35-71-65-65-71-73-67-61-73-7A")]
+        [InlineData("nfbd05c6kaas8oxa", "00-15-00-10-6E-66-62-64-30-35-63-36-6B-61-61-73-38-6F-78-61")]
+        public void Nonce_Convert_To_ByteArray(string nonce, string expect)
+        {
+            var nonceObj = new Nonce(nonce);
+            var byteArray = nonceObj.ToByte();
+            var byteArrayString = BitConverter.ToString(byteArray);
+            Assert.Equal(expect, byteArrayString);
+        }
+
+        [Fact]
+        public void Random_Nonce_Convert_To_ByteArray()
+        {
+            var nonceObj = new Nonce();
+            var byteArray = nonceObj.ToByte();
+            var byteArrayString = BitConverter.ToString(byteArray);
+            Assert.Equal(20, byteArray.Length);
+            Assert.StartsWith("00-15-00-10", byteArrayString);
+        }
+    }
+}
