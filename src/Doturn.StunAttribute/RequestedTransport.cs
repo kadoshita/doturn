3eee5817f00690f345b8c259ff4202858964d685
kadoshita <sublimer.me@gmail.com>
2021-12-26 20:36:45 +0900

    Add RequestedTransport stun attribute

---
+++ src/Doturn.StunAttribute/RequestedTransport.cs
@@ -0,0 +1,38 @@
+using System;
+using Doturn.Common;
+
+namespace Doturn.StunAttribute
+{
+    public class RequestedTransport : StunAttributeBase
+    {
+        public readonly Type type = Type.REQUESTED_TRANSPORT;
+        public readonly Transport transport;
+        private readonly byte[] reserved;
+        public override Type Type => this.type;
+
+        public RequestedTransport()
+        {
+            this.transport = Transport.UDP;
+            this.reserved = new byte[] { 0x00, 0x00, 0x00 };
+        }
+        public RequestedTransport(Transport transport, byte[] reserved)
+        {
+            this.transport = transport;
+            this.reserved = reserved;
+        }
+        public override byte[] ToByte()
+        {
+            var typeByteArray = this.type.ToByte();
+            var transportByteArray = this.transport.ToByte();
+            var length = transportByteArray.Length + this.reserved.Length;
+            var lengthByteArray = BitConverter.GetBytes((Int16)length);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(lengthByteArray);
+            }
+            var res = new byte[2 + 2 + length];
+            ByteArrayUtils.MergeByteArray(ref res, typeByteArray, lengthByteArray, transportByteArray, this.reserved);
+            return res;
+        }
+    }
+}
