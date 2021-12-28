a71948f098ea581c3d46b123fd7563999cf257c1
kadoshita <sublimer.me@gmail.com>
2021-12-28 21:59:17 +0900

    Update MergeByteArray option

--- src/Doturn.Common/ByteArrayUtils.cs
+++ src/Doturn.Common/ByteArrayUtils.cs
@@ -6,7 +6,16 @@ namespace Doturn.Common
     {
         public static void MergeByteArray(ref byte[] res, params byte[][] data)
         {
-            int endPos = 0;
+            _MergeByteArray(ref res, 0, data);
+        }
+        public static void MergeByteArray(ref byte[] res, int offset, params byte[][] data)
+        {
+            _MergeByteArray(ref res, offset, data);
+        }
+
+        private static void _MergeByteArray(ref byte[] res, int offset, params byte[][] data)
+        {
+            int endPos = offset;
             foreach (var datum in data)
             {
                 Buffer.BlockCopy(datum, 0, res, endPos, datum.Length);
