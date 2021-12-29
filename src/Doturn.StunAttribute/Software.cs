d6e59df775cb0ba6aa792006b3ec453c9979405d
kadoshita <sublimer.me@gmail.com>
2021-12-29 23:05:45 +0900

    Add software stun attribute

---
+++ src/Doturn.StunAttribute/Software.cs
@@ -0,0 +1,45 @@
+using System;
+using Doturn.Common;
+
+namespace Doturn.StunAttribute
+{
+    public class Software : StunAttributeBase
+    {
+        public readonly Type type = Type.SOFTWARE;
+        public readonly string software;
+        public override Type Type => this.type;
+        public Software()
+        {
+            this.software = "Doturn";
+        }
+        public Software(string software)
+        {
+            this.software = software;
+        }
+
+        public override byte[] ToByte()
+        {
+            var typeByteArray = this.type.ToByte();
+            var softwareByteArray = System.Text.Encoding.ASCII.GetBytes(this.software);
+            var length = softwareByteArray.Length;
+            var paddingLength = 8 - ((2 + 2 + length) % 8);
+            if (paddingLength >= 8)
+            {
+                paddingLength = 0;
+            }
+            var lengthByteArray = BitConverter.GetBytes((Int16)length);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(lengthByteArray);
+            }
+            var res = new byte[2 + 2 + length + paddingLength];
+            ByteArrayUtils.MergeByteArray(ref res, typeByteArray, lengthByteArray, softwareByteArray);
+            byte[] padding = { 0 };
+            for (var i = 0; i < paddingLength; i++)
+            {
+                ByteArrayUtils.MergeByteArray(ref res, res.Length - paddingLength, padding);
+            }
+            return res;
+        }
+    }
+}
