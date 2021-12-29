396bbb60b8941f099b14f0254c590016410cbf6c
kadoshita <sublimer.me@gmail.com>
2021-12-29 23:06:01 +0900

    Add fingerprint stun attribute

---
+++ src/Doturn.StunAttribute/Fingerprint.cs
@@ -0,0 +1,48 @@
+using System;
+using Doturn.Common;
+using Force.Crc32;
+
+namespace Doturn.StunAttribute
+{
+    public class Fingerprint : StunAttributeBase
+    {
+        public readonly Type type = Type.FINGERPRINT;
+        private readonly byte[] crc32;
+        public override Type Type => this.type;
+
+        public Fingerprint(ref byte[] data)
+        {
+            var crc32 = Crc32Algorithm.Compute(data, 0, data.Length);
+            var crc32Byte = BitConverter.GetBytes(crc32);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(crc32Byte);
+            }
+            var crc32XorByte = new byte[crc32Byte.Length];
+            var fingerprintXor = BitConverter.GetBytes(0x5354554e);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(fingerprintXor);
+            }
+            for (int i = 0; i < crc32Byte.Length; i++)
+            {
+                crc32XorByte[i] = (byte)(crc32Byte[i] ^ fingerprintXor[i]);
+            }
+            this.crc32 = crc32XorByte;
+        }
+        public override byte[] ToByte()
+        {
+            var typeByteArray = this.type.ToByte();
+            var length = this.crc32.Length;
+            var lengthByteArray = BitConverter.GetBytes((Int16)length);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(lengthByteArray);
+            }
+
+            var res = new byte[2 + 2 + length];
+            ByteArrayUtils.MergeByteArray(ref res, typeByteArray, lengthByteArray, this.crc32);
+            return res;
+        }
+    }
+}
