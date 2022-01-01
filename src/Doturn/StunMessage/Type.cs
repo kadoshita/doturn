631f07616af7618f171bd1372b9fee14897cc9f4
kadoshita <sublimer.me@gmail.com>
2022-01-01 12:58:51 +0900

    Add some stun message type

--- src/Doturn/StunMessage/Type.cs
+++ src/Doturn/StunMessage/Type.cs
@@ -6,13 +6,16 @@ namespace Doturn.StunMessage
     {
         BINDING = 0x0001,
         BINDING_SUCCESS = 0x0101,
+        BINDING_ERROR = 0x0111,
         ALLOCATE = 0x0003,
-        REFRESH = 0x0004,
-        CREATE_PERMISSION = 0x0008,
-        REFRESH_SUCCESS = 0x0104,
         ALLOCATE_SUCCESS = 0x0103,
-        CREATE_PERMISSION_SUCCESS = 0x0108,
         ALLOCATE_ERROR = 0x0113,
+        REFRESH = 0x0004,
+        REFRESH_SUCCESS = 0x0104,
+        REFRESH_ERROR = 0x0114,
+        CREATE_PERMISSION = 0x0008,
+        CREATE_PERMISSION_SUCCESS = 0x0108,
+        CREATE_PERMISSION_ERROR = 0x0118,
     }
     public static class StunMessageTypeExtends
     {
--- test/Doturn.Test/StunMessage/TypeTest.cs
+++ test/Doturn.Test/StunMessage/TypeTest.cs
@@ -8,13 +8,16 @@ namespace Doturn.StunMessage.Test
         [Theory]
         [InlineData(StunMessage.Type.BINDING, "00-01")]
         [InlineData(StunMessage.Type.BINDING_SUCCESS, "01-01")]
+        [InlineData(StunMessage.Type.BINDING_ERROR, "01-11")]
         [InlineData(StunMessage.Type.ALLOCATE, "00-03")]
-        [InlineData(StunMessage.Type.REFRESH, "00-04")]
-        [InlineData(StunMessage.Type.CREATE_PERMISSION, "00-08")]
-        [InlineData(StunMessage.Type.REFRESH_SUCCESS, "01-04")]
         [InlineData(StunMessage.Type.ALLOCATE_SUCCESS, "01-03")]
-        [InlineData(StunMessage.Type.CREATE_PERMISSION_SUCCESS, "01-08")]
         [InlineData(StunMessage.Type.ALLOCATE_ERROR, "01-13")]
+        [InlineData(StunMessage.Type.REFRESH, "00-04")]
+        [InlineData(StunMessage.Type.REFRESH_SUCCESS, "01-04")]
+        [InlineData(StunMessage.Type.REFRESH_ERROR, "01-14")]
+        [InlineData(StunMessage.Type.CREATE_PERMISSION, "00-08")]
+        [InlineData(StunMessage.Type.CREATE_PERMISSION_SUCCESS, "01-08")]
+        [InlineData(StunMessage.Type.CREATE_PERMISSION_ERROR, "01-18")]
         public void StunMessageType_Convert_To_ByteArray(StunMessage.Type type, string byteArrayString)
         {
             var byteArray = type.ToByte();
