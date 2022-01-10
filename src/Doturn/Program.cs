0dd062747c57f9812d7fa2b182bdd7eaa13b4a15
kadoshita <sublimer.me@gmail.com>
2022-01-10 16:10:45 +0900

    Update to use Generic Host

--- src/Doturn/Program.cs
+++ src/Doturn/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Doturn
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+        .ConfigureLogging(logging =>
+        {
+            logging.ClearProviders();
+            logging.AddSimpleConsole(options =>
+            {
+                options.SingleLine = true;
+                options.TimestampFormat = "yyyy/MM/dd hh:mm:ss.fff ";
+            });
+            logging.SetMinimumLevel(LogLevel.Debug);
+        })
+        .ConfigureServices((hostContext, services) =>
+        {
+            services.AddHostedService<StunServerService.StunServerService>();
+            services.Configure<AppSettings>(hostContext.Configuration);
+        });
     }
 }
