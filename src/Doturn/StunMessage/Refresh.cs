f15b51e016393103f01c133ea0db795d7605a3a0
kadoshita <sublimer.me@gmail.com>
2022-01-30 15:09:49 +0900

    Update to use connection manager

--- src/Doturn/StunMessage/Refresh.cs
+++ src/Doturn/StunMessage/Refresh.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using Doturn.StunAttribute;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace Doturn.StunMessage
 {
--- src/Doturn/StunServerService/StunServerService.cs
+++ src/Doturn/StunServerService/StunServerService.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
+using Doturn.Network;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -13,24 +14,42 @@ namespace Doturn.StunServerService
     {
         private readonly ILogger<StunServerService> _logger;
         private readonly IOptions<AppSettings> _options;
+        private readonly IConnectionManager _connectionManager;
+        private readonly IPortAllocator _portAllocator;
         public readonly ushort listenPort;
         private readonly UdpClient _client;
 
-        public StunServerService(ILogger<StunServerService> logger, IOptions<AppSettings> options)
+        public StunServerService(ILogger<StunServerService> logger, IOptions<AppSettings> options, IConnectionManager connectionManager, IPortAllocator portAllocator)
         {
             _logger = logger;
             _options = options;
+            _connectionManager = connectionManager;
+            _portAllocator = portAllocator;
             listenPort = options.Value.ListeningPort;
             var endpoint = new IPEndPoint(IPAddress.Any, listenPort);
             _client = new UdpClient(endpoint);
         }
-
+        public StunServerService(ILogger<StunServerService> logger, IOptions<AppSettings> options, IConnectionManager connectionManager, IPortAllocator portAllocator, ushort listenPort)
+        {
+            _logger = logger;
+            _options = options;
+            _connectionManager = connectionManager;
+            _portAllocator = portAllocator;
+            this.listenPort = listenPort;
+            var endpoint = new IPEndPoint(IPAddress.Any, listenPort);
+            _client = new UdpClient(endpoint);
+        }
         protected override async Task ExecuteAsync(CancellationToken cancellationToken)
         {
+            _logger.LogDebug($"Listening on {listenPort}");
             while (true)
             {
-                _logger.LogDebug("Wait...");
                 UdpReceiveResult data = await _client.ReceiveAsync();
+                if (data.Buffer.Length < 20)
+                {
+                    _logger.LogDebug("Unknown data received");
+                    continue;
+                }
                 try
                 {
                     StunMessage.IStunMessage message = StunMessage.StunMessageParser.Parse(data.Buffer, _options.Value);
@@ -43,7 +62,12 @@ namespace Doturn.StunServerService
                     }
                     else if (message.Type == StunMessage.Type.ALLOCATE)
                     {
-                        byte[] res = ((StunMessage.Allocate)message).CreateSuccessResponse(data.RemoteEndPoint);
+                        IPAddress relayAddress = IPAddress.Parse(_options.Value.ExternalIPAddress);
+                        ushort relayPort = _portAllocator.GetPort();
+                        _connectionManager.AddConnectionEntry(new ConnectionEntry(relayAddress, relayPort, data.RemoteEndPoint.Address, (ushort)data.RemoteEndPoint.Port));
+                        var sss = new StunServerService(_logger, _options, _connectionManager, _portAllocator, relayPort);
+                        _ = sss.ExecuteAsync(new CancellationToken());
+                        byte[] res = ((StunMessage.Allocate)message).CreateSuccessResponse(data.RemoteEndPoint, relayAddress, relayPort);
                         _logger.LogDebug($"res: {message.Type} {BitConverter.ToString(res)}");
                         await _client.SendAsync(res, res.Length, data.RemoteEndPoint);
                     }
@@ -68,7 +92,7 @@ namespace Doturn.StunServerService
                 }
                 catch (StunMessage.StunMessageParseException)
                 {
-                    _logger.LogDebug("Unknown data received");
+                    _logger.LogDebug($"Unknown data received: {BitConverter.ToString(data.Buffer)}");
                 }
             }
         }
