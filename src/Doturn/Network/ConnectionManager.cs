44d11b6a550840cf437190456592158858fba105
kadoshita <sublimer.me@gmail.com>
2022-02-05 14:38:24 +0900

    Update to add channel number

--- src/Doturn/Network/ConnectionManager.cs
+++ src/Doturn/Network/ConnectionManager.cs
@@ -1,49 +1,136 @@
+using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
 namespace Doturn.Network
 {
     public class ConnectionEntry
     {
-        public IPAddress address { get; set; }
-        public ushort port { get; set; }
-        public IPAddress remoteAddress { get; set; }
-        public ushort remotePort { get; set; }
+        public IPEndPoint client { get; set; }
+        public IPEndPoint? peer { get; set; }
+        public byte[]? channelNumber { get; set; }
+        public StunServerService.StunServerService sss { get; set; }
 
-        public ConnectionEntry(IPAddress address, ushort port, IPAddress remoteAddress, ushort remotePort)
+        public ConnectionEntry(IPAddress clientAddress, ushort clientPort, StunServerService.StunServerService sss)
         {
-            this.address = address;
-            this.port = port;
-            this.remoteAddress = remoteAddress;
-            this.remotePort = remotePort;
+            client = new IPEndPoint(clientAddress, clientPort);
+            this.sss = sss;
+        }
+        public ConnectionEntry(IPEndPoint client, StunServerService.StunServerService sss)
+        {
+            this.client = client;
+            this.sss = sss;
         }
     }
 
     public interface IConnectionManager
     {
-
+        void SetMainClient(UdpClient client);
+        Task<int> SendMainClientAsync(byte[] data, int length, IPEndPoint endpoint);
         void AddConnectionEntry(ConnectionEntry entry);
+        void AddPeerEndpoint(IPEndPoint client, IPEndPoint peer);
+        void AddChannelNumber(IPEndPoint client, byte[] channelNumber);
+        ConnectionEntry GetEntry(IPEndPoint endpoint);
+        ConnectionEntry GetEntryByPeer(IPEndPoint endpoint);
+        ConnectionEntry GetEntryByChannelNumber(byte[] channelNumber);
         int GetEntriesCount();
+
     }
     public class ConnectionManager : IConnectionManager
     {
         public readonly List<ConnectionEntry> _entries = new();
         private readonly ILogger<ConnectionManager> _logger;
 
+        public UdpClient mainClient;
+
         public ConnectionManager(ILogger<ConnectionManager> logger)
         {
             _logger = logger;
         }
+
+        public void SetMainClient(UdpClient client)
+        {
+            if (mainClient == null)
+            {
+                mainClient = client;
+            }
+        }
+
+        public Task<int> SendMainClientAsync(byte[] data, int length, IPEndPoint endpoint)
+        {
+            return mainClient.SendAsync(data, length, endpoint);
+        }
         public void AddConnectionEntry(ConnectionEntry entry)
         {
-            _logger.LogDebug($"Add entry {entry.remoteAddress}:{entry.remotePort} - {entry.address}:{entry.port}");
+            _logger.LogDebug($"Add entry {entry.client.Address}:{entry.client.Port}");
             _entries.Add(entry);
             _logger.LogDebug($"Entries: {_entries.Count}");
         }
+
+        public void AddPeerEndpoint(IPEndPoint client, IPEndPoint peer)
+        {
+            _logger.LogDebug($"Add peer {client.Address}:{client.Port} - {peer.Address}:{peer.Port}");
+            ConnectionEntry entry = _entries.Find(e => e.client.Equals(client));
+            entry.peer = peer;
+        }
+
+        public void AddChannelNumber(IPEndPoint client, byte[] channelNumber)
+        {
+            _logger.LogDebug($"Add channel number {client.Address}:{client.Port} - {BitConverter.ToString(channelNumber)}");
+            ConnectionEntry entry = _entries.Find(e => e.client.Equals(client));
+            entry.channelNumber = channelNumber;
+        }
+
         public int GetEntriesCount()
         {
             return _entries.Count;
+        }
+
+        public ConnectionEntry GetEntry(IPEndPoint endpoint)
+        {
+            _logger.LogDebug($"Get entry {endpoint.Address} {endpoint.Port}");
+            ConnectionEntry entry = _entries.Find(e => e.client.Equals(endpoint));
+            _logger.LogDebug($"Entry: {entry}");
+            return entry;
+        }
+
+        public ConnectionEntry GetEntryByPeer(IPEndPoint endpoint)
+        {
+            _logger.LogDebug($"Get entry by Peer {endpoint.Address} {endpoint.Port}");
+            ConnectionEntry entry = _entries.Find(e =>
+            {
+                if (e.peer == null)
+                {
+                    return false;
+                }
+                return e.peer.Equals(endpoint);
+            });
+            if (entry != null)
+            {
+                _logger.LogDebug($"Entry {entry.client.Address}:{entry.client.Port} - {entry.peer.Address}:{entry.peer.Port}");
+            }
+            return entry;
+        }
+
+        public ConnectionEntry GetEntryByChannelNumber(byte[] channelNumber)
+        {
+            _logger.LogDebug($"Get entry by Channel Number {BitConverter.ToString(channelNumber)}");
+            ConnectionEntry entry = _entries.Find(e =>
+            {
+                if (e.channelNumber == null)
+                {
+                    return false;
+                }
+                return e.channelNumber[0] == channelNumber[0] && e.channelNumber[1] == channelNumber[1];
+            });
+            if (entry != null)
+            {
+                _logger.LogDebug($"Entry {entry.client.Address}:{entry.client.Port} - {entry.peer.Address}:{entry.peer.Port}");
+            }
+            return entry;
         }
     }
 }
