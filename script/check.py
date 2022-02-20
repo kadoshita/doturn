7fc59fd318da0cebbac2209824eac3beb247a7c7
kadoshita <sublimer.me@gmail.com>
2022-02-20 14:38:49 +0900

    Update turn send e2e test config

--- script/check.py
+++ script/check.py
@@ -76,7 +76,6 @@ def main():
     byte = ''
     lost_packets = ''
     for line in sys.stdin:
-        print(line)
         if 'Average round trip delay' in line:
             rtt = line
         if 'Average jitter' in line:
